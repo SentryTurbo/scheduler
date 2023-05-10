a5cae7e119ae0b60b7b7cab686e2b23c825f5bcf
Vlads Muravjovs <vlads.muravjovs@gmail.com>
2023-05-10 12:39:18 +0300

    Auth token test

--- components/Windows/CreateProjectWindow.js
+++ components/Windows/CreateProjectWindow.js
@@ -3,17 +3,25 @@ import React from "react";
 import {FormGeneric, InputButton, InputGeneric} from "../Modules/FormModules";
 
 export default function CreateProjectWindow(props){
-    const _submit = (data) => {
-        fetch("http://localhost:80/scheduler/actions/createproject.php", {method:'post', body:data})
-            .then(res => res.json())
-            .then((result) => {
-                console.log(result);
-                props.refresh();
-                props.setWindow(null);
-            },
-            (error) => {
-                console.log(error);
-            })
+    const _submit = async (data) => {
+        var sendData = data;
+        sendData.append("auth", localStorage.getItem("auth"));
+        
+        const endpoint = 'http://localhost:80/scheduler/actions/createproject.php';
+
+        const options = {
+            method:'POST',
+            body: sendData,
+        };
+
+        const response = await fetch(endpoint,options);
+
+        const result = await response.text();
+
+        console.log(result);
+
+        props.refresh();
+        props.setWindow(null);
     }
     
     const dataset = [
--- components/Windows/Profile.js
+++ components/Windows/Profile.js
@@ -1,9 +1,11 @@
-import react from "react";
+import react, { useEffect, useState } from "react";
 
 import { InputButton } from "../Modules/FormModules";
 import { useRouter } from "next/router";
 
 export default function ProfileWindow(props){
+    const [userData, setUser] = useState({username:'unknown'});
+    
     const router = useRouter();
 
     const logout = () => {
@@ -13,11 +15,37 @@ export default function ProfileWindow(props){
 
         router.push('/');
     }
+
+    const requestUserData = async () => {
+        const JSONdata = await localStorage.getItem("auth");
+
+        const endpoint = 'http://localhost:80/scheduler/profile.php';
+
+        const options = {
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json',
+            },
+            body: JSONdata,
+        };
+
+        const response = await fetch(endpoint,options);
+
+        const result = await response.json();
+
+        setUser(result);
+
+        console.log(result);
+    }
+
+    useEffect(()=>{
+        requestUserData();
+    }, []);
     
     return (
         <div>
             <h3>Profile</h3>
-            <p>username: user</p>
+            <p>username: {userData.username}</p>
             <InputButton label="log out" onClick={logout} />
         </div>
     )
