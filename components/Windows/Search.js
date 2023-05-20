19e1f9db02bee0c3a6a7be74b53fc837d3ce493e
Vlads Muravjovs <vlads.muravjovs@gmail.com>
2023-05-20 10:05:17 +0300

    Search filters

--- components/Windows/Search.js
+++ components/Windows/Search.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { InputButton } from "../Modules/FormModules";
+import { BsFilter } from "react-icons/bs";
 import Link from "next/link";
 
 import styles from "../../styles/Inputs.module.css";
@@ -13,11 +14,23 @@ const translations = {
 
 export default function SearchWindow(p){
     const [query, setQuery] = useState({
-        text:''
+        text:'',
+        filters:{
+            project:true,
+            milestone:true,
+            assignment:true,
+            submission:true
+        }
     });
 
     const [result, setResult] = useState([]);
     const [loading,setLoading] = useState(false);
+
+    const [filter, setFilter] = useState(false);
+
+    const toggleFilter = () => {
+        setFilter(!filter);
+    }
 
     const handleTextChange = (e) => {
         setQuery({...query, ['text']:e.target.value});
@@ -28,6 +41,8 @@ export default function SearchWindow(p){
             query:query,
             auth:localStorage.getItem("auth")
         }
+
+        console.log(queryData);
 
         const endpoint = "http://localhost:80/scheduler/actions/search.php";
 
@@ -55,9 +70,20 @@ export default function SearchWindow(p){
                 <div style={{textAlign:'center', fontSize:'1.25em', fontWeight:'bold'}}>
                     Meklēšana
                 </div>
-                <div style={{marginTop:25, width:'70%'}}>
+                <div style={{marginTop:25, display:'grid', gridTemplateColumns:'auto 30px', width:'70%', gap:10 }}>
                     <input placeholder="Vaicājums..." className={styles['input-generic']} style={{width:'100%'}} type="text" onChange={handleTextChange} value={query.text}/>
+                    <div onClick={toggleFilter} className={styles['input-generic']} style={{display:'flex', flexWrap:'wrap', placeContent:'center', borderBottom:'solid', borderWidth:1, height:'auto'}}>
+                        <BsFilter style={{fontSize:'1.5em'}}/>
+                    </div>
                 </div>
+                {filter &&
+                    <div style={{display:'flex', justifyContent:'space-between', marginTop:10}}>
+                        <FilterPick setValues={setQuery} query={query} values={query.filters} name="project" label="Projekti"/>
+                        <FilterPick setValues={setQuery} query={query} values={query.filters} name="milestone" label="Mērķi"/>
+                        <FilterPick setValues={setQuery} query={query} values={query.filters} name="assignment" label="Uzdevumi"/>
+                        <FilterPick setValues={setQuery} query={query} values={query.filters} name="submission" label="Risinājumi"/>
+                    </div>
+                }
                 <div style={{marginTop:10, display:'flex', justifyContent:'center'}}>
                     <InputButton onClick={requestQuery} label="Meklēt!"/>
                 </div>
@@ -95,5 +121,18 @@ function SearchResult(p){
                 <div style={{height:'fit-content'}}>{translations[p.d.type]}</div>
             </div>
         </Link>
+    )
+}
+
+function FilterPick(p){
+    const toggleValue = (e) => {
+        p.setValues({...p.query, ['filters']:{...p.query.filters, [p.name]:!p.query.filters[p.name]}})
+    }
+
+    return (
+        <div style={{display:'flex', alignContent:'center', flexWrap:'wrap'}} onClick={toggleValue}>
+            <input type="checkbox" checked={p.values[p.name]}></input>
+            <div style={{fontSize:'0.8em'}}>{p.label}</div>
+        </div>
     )
 }
